8870c2b8f14f0b6704982c9cf2b098d9692fe10f
Maksim Tsatsura <maxim.tsatsura@gmail.com>
2023-11-11 11:29:25 +0300

    feat: add guests

--- script.js
+++ script.js
@@ -1,17 +1,5 @@
-const names = [
-  { id: '0', names: 'Общее' },
-  { id: '1', names: 'Дорогой Гоша' }, //ты
-  { id: '2', names: 'Дорогие Ань и Виктория' },
-  { id: '3', names: 'Дорогие Илья и Ольга' },
-  { id: '4', names: 'Уважаемый Владос' }, //ты
-  { id: '5', names: 'Дорогая крестная' },
-  { id: '6', names: 'Дорогой крестный' },
-];
-
 const main = document.querySelector('main');
-const invitedNames = document.getElementById('invited-names');
-const invitedText = document.getElementById('invited-text');
-const invitedMessage = document.getElementById('invited-message');
+
 const img1 = document.getElementById('img1');
 const img2 = document.getElementById('img2');
 // const img3 = document.getElementById('img3');
@@ -23,17 +11,14 @@ const id = urlParams.get('id');
 
 const baseImgPath = './img/guests/';
 
-if (+id && !isNaN(+id) && (+id == 2 || +id == 3 || +id == 5 || +id == 6)) { 
-  img1.src = baseImgPath + `1_${id}.jpg`;
-  // img2.src = baseImgPath + `2_${id}.jpg`;
+if (+id && !isNaN(+id) && (+id <= 52) && (+id > 0)) { 
+  // img1.src = baseImgPath + `1_${id}.jpg`;
+  img2.src = baseImgPath + `2_${id}.jpg`;
   // img3.src = baseImgPath + '3.jpg';
   // img4.src = baseImgPath + '4.jpg';
-} else if (+id && !isNaN(+id) && (+id == 1 || +id == 4 )) {
-  img1.src = baseImgPath + `1_${id}.jpg`;
-  img2.src = `./img/2_1.jpg`;
 }
 else {
- 
+  img2.src = `./img/2.jpg`;
 }
 
 const imgs = [...document.querySelectorAll('.img-wrapper img')];
